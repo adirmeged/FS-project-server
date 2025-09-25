c81d08e8fbaa663c49052a56de214d3494c07a84
shaharizhak <shaharizhak811@gmail.com>
2025-09-25 11:30:31 +0300

    גה

--- server/src/features/movies/Routes.ts
+++ server/src/features/movies/Routes.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 //movies/
 router.get("/", Controller.getAllMovie);
 router.get("/lastMovie", Controller.getLastMovie);
+router.get("/topReview", Controller.getTopReview);
 router.get("/:id", Controller.getMovieById);
 
 export default router;
